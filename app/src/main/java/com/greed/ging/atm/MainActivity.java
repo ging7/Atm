1ffcacf9253f647fca35ee51e7fb4d7cbe7eeb8c
ging7 <g776808@gmail.com>
2018-04-07 22:41:35 +0800

    Menu

--- app/src/main/java/com/greed/ging/atm/MainActivity.java
+++ app/src/main/java/com/greed/ging/atm/MainActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int RC_LOGIN = 1; //新增一個類別層級常數，代表登入功能
     boolean logon = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(intent, RC_LOGIN);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_setting){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
