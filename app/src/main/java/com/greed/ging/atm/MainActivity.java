9667808203eb59e9aa1d735ab91c02af1c5eea21
ging7 <g776808@gmail.com>
2018-04-07 12:20:03 +0800

    回傳Activity Result

--- app/src/main/java/com/greed/ging/atm/MainActivity.java
+++ app/src/main/java/com/greed/ging/atm/MainActivity.java
@@ -3,9 +3,11 @@ package com.greed.ging.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int RC_LOGIN = 1; //新增一個類別層級常數，代表登入功能
     boolean logon = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +16,22 @@ public class MainActivity extends AppCompatActivity {
         if (!logon){
             //如果未登入，則開啟LoginActivity
             Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+           // startActivity(intent);
+            startActivityForResult(intent, RC_LOGIN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_LOGIN){
+            if (resultCode == RESULT_OK){
+                String uid = data.getStringExtra("LOGIN_USERID");
+                String pw = data.getStringExtra("LOGIN_PASSWD");
+                Log.d("RESULT", uid + "/" + pw);
+            }else{
+                finish();
+            }
         }
     }
 }
