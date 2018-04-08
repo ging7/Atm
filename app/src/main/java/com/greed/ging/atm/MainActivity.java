929af723796e8a7113ccb71edc08d1ceaddacf05
ging7 <g776808@gmail.com>
2018-04-08 11:50:04 +0800

    Spinner

--- app/src/main/java/com/greed/ging/atm/MainActivity.java
+++ app/src/main/java/com/greed/ging/atm/MainActivity.java
@@ -6,12 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView lv_atm;
+    Spinner notify;
     public static final int RC_LOGIN = 1; //新增一個類別層級常數，代表登入功能
     boolean logon = false;
     String[] func = {"餘額查詢", "交易明細", "最新消息", "理財投資", "離開"};
@@ -20,11 +25,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //使用spinner
+        notify = this.findViewById(R.id.spin_atm);
+        final ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(this, R.array.notify_array, android.R.layout.simple_spinner_item);
+        notify.setAdapter(nAdapter);
+        notify.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, nAdapter.getItem(position).toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         //使用ListView
         lv_atm = this.findViewById(R.id.lv_atm);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, func);
+        nAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         lv_atm.setAdapter(adapter);
-
         //測試TestActivity
         //startActivity(new Intent(this, TestActivity.class));
         if (!logon){
