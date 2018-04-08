533953120dc54670291a6538b7bf835128caea99
ging7 <g776808@gmail.com>
2018-04-08 21:28:25 +0800

    客製化圖案GridView

--- app/src/main/java/com/greed/ging/atm/MainActivity.java
+++ app/src/main/java/com/greed/ging/atm/MainActivity.java
@@ -7,11 +7,15 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
@@ -22,6 +26,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public static final int RC_LOGIN = 1; //新增一個類別層級常數，代表登入功能
     boolean logon = false;
     String[] func = {"餘額查詢", "交易明細", "最新消息", "理財投資", "離開"};
+    int[] icons = {R.drawable.func_balance,
+            R.drawable.func_history,
+            R.drawable.func_news,
+            R.drawable.func_finance,
+            R.drawable.func_exit};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         //使用GridView
         gv_atm = findViewById(R.id.gv_atm);
-        ArrayAdapter gAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, func);
+        IconAdapter gAdapter = new IconAdapter();
         gv_atm.setAdapter(gAdapter);
         gv_atm.setOnItemClickListener(this);
         //使用spinner
@@ -60,6 +69,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             Intent intent = new Intent(this, LoginActivity.class);
            // startActivity(intent);
             startActivityForResult(intent, RC_LOGIN);
+        }
+    }
+
+    class IconAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return func.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return func[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return icons[position];
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = convertView;
+            if (row == null){
+                row = getLayoutInflater().inflate(R.layout.item_row, null);
+                ImageView image = row.findViewById(R.id.img_item);
+                TextView text = row.findViewById(R.id.txt_item);
+                image.setImageResource(icons[position]);
+                text.setText(func[position]);
+            }
+            return row;
         }
     }
 
