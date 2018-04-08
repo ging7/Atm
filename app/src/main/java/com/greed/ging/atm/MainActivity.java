21ed74c3a75b8d2423db88a3cc4ba4e0b51e8ca3
ging7 <g776808@gmail.com>
2018-04-08 11:09:57 +0800

    ListView

--- app/src/main/java/com/greed/ging/atm/MainActivity.java
+++ app/src/main/java/com/greed/ging/atm/MainActivity.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lv_atm;
     public static final int RC_LOGIN = 1; //新增一個類別層級常數，代表登入功能
     boolean logon = false;
-
+    String[] func = {"餘額查詢", "交易明細", "最新消息", "理財投資", "離開"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //使用ListView
+        lv_atm = this.findViewById(R.id.lv_atm);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, func);
+        lv_atm.setAdapter(adapter);
+
         //測試TestActivity
         //startActivity(new Intent(this, TestActivity.class));
         if (!logon){
