767baefb1c90cc22a65c46468234031b4e29b664
ging7 <g776808@gmail.com>
2018-04-15 15:43:10 +0800

    SimpleCursorAdapter方法

--- app/src/main/java/com/greed/ging/atm/FinanceActivity.java
+++ app/src/main/java/com/greed/ging/atm/FinanceActivity.java
@@ -1,12 +1,15 @@
 package com.greed.ging.atm;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class FinanceActivity extends AppCompatActivity {
 
@@ -16,6 +19,11 @@ public class FinanceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_finance);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ListView list = (ListView) findViewById(R.id.list);
+        MyDBHelper helper = new MyDBHelper(this, "expense.db", null, 1);
+        Cursor c = helper.getReadableDatabase().query("exp", null, null, null, null, null, null);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_expandable_list_item_2, c, new String[] {"info", "amount"}, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        list.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
